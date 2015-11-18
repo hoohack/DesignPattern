126c0b8e96e355cbd88478fbdd174b3ba6a9b53b
HectorHu <hhqcontinue@gmail.com>
2015-11-18 18:26:07 +0800

    update

--- Decorator/PHP/Mocha.php
+++ Decorator/PHP/Mocha.php
@@ -1,17 +1,17 @@
 <?php
   class Mocha extends CondimentDecorator {
-    public $_beverage;
+    public $beverage;
 
     public function __construct($beverage) {
       $this->description = ' Mocha';
-      $this->_beverage = $beverage;
+      $this->beverage = $beverage;
     }
 
     public function cost() {
-      return $this->_beverage->cost() + 10.3;
+      return $this->beverage->cost() + 10.3;
     }
 
     public function getDescription() {
-      return $this->_beverage->getDescription() . $this->description;
+      return $this->beverage->getDescription() . $this->description;
     }
   }
